0b7ac1f354ebe3ecbe2f5b9dd9457d9013ec0a65
HaveNiceDa <2676424029@qq.com>
2026-01-13 21:25:41 +0800

    '打通convex和clerk的连接

---
+++ src/components/providers.tsx
@@ -0,0 +1,28 @@
+'use client';
+
+import { Authenticated, AuthLoading, ConvexReactClient, Unauthenticated } from 'convex/react';
+import { ClerkProvider, SignInButton, SignUpButton, useAuth, UserButton } from '@clerk/nextjs';
+import { ConvexProviderWithClerk } from 'convex/react-clerk';
+import { ThemeProvider } from './theme-provider';
+
+const convex = new ConvexReactClient(process.env.NEXT_PUBLIC_CONVEX_URL!);
+
+export const Providers = ({ children }: { children: React.ReactNode }) => {
+	return (
+		<ClerkProvider>
+			<ConvexProviderWithClerk client={convex} useAuth={useAuth}>
+				<ThemeProvider attribute="class" defaultTheme="dark" enableSystem disableTransitionOnChange>
+					<Authenticated>
+						<UserButton />
+					</Authenticated>
+					<Unauthenticated>
+						<SignInButton />
+						<SignUpButton />
+					</Unauthenticated>
+					<AuthLoading>Auth Loading...</AuthLoading>
+					{children}
+				</ThemeProvider>
+			</ConvexProviderWithClerk>
+		</ClerkProvider>
+	);
+};
